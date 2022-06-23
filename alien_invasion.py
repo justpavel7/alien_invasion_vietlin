2856f11f588f635c40b51827ad05a9288891cc83
Pavel Vietlin <justpave@gmail.com>
2022-06-23 16:04:48 +0300

    main game loop and game creation

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,3 +11,19 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """"Запуск основного цикла игры."""
+
+        while True:
+            # Отслеживание клавиш и мыши
+            for event in pygame.event.get():
+                if event.type = pygame.QUIT:
+                    sys.exit()
+            # Отображение последнего прорисованого экрана
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    # Создание экземпляра и создание игры
+    ai = AlienInvasion()
+    ai.run_game()
